57734d9dc125321d713deba76b7f39c076f36da0
Anhzf <36941633+anhzf@users.noreply.github.com>
2021-04-25 21:17:44 +0700

    fix: :sparkles: fix PRAKTIKUM02 numbering

--- PRAKTIKUM02/prak020202.php
+++ PRAKTIKUM02/prak020202.php
@@ -2,17 +2,16 @@
 
 $row = 4;
 $col = 5;
+$numb = 1;
 
 ?>
 
 <table border="1">
     <?php for ($i = 0; $i < $row; $i++) { ?>
         <tr>
-            <?php for ($j = 0; $j < $col; $j++) {
-                $numb = ($i + 1) * ($j + 1);
-            ?>
+            <?php for ($j = 0; $j < $col; $j++) { ?>
                 <td><?= $numb ?></td>
-            <?php } ?>
+            <?php $numb++; } ?>
         </tr>
     <?php } ?>
 </table>
--- PRAKTIKUM02/prak020203.php
+++ PRAKTIKUM02/prak020203.php
@@ -3,18 +3,18 @@ require './lib.php';
 
 $row = 4;
 $col = 5;
+$numb = 1;
 
 ?>
 <table border="1">
     <?php for ($i = 0; $i < $row; $i++) { ?>
         <tr>
             <?php for ($j = 0; $j < $col; $j++) {
-                $numb = ($i + 1) * ($j + 1);
                 $fontColor = isEven($numb) ? 'white' : 'red';
                 $bgColor = isEven($numb) ? 'red' : 'white';
             ?>
                 <td style="background-color: <?= $bgColor ?>; color: <?= $fontColor ?>;"><?= $numb ?></td>
-            <?php } ?>
+            <?php $numb++; } ?>
         </tr>
     <?php } ?>
 </table>
